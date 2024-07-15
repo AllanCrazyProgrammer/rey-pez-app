39ee5b92b44941c4e2380f06181d26b5c1811c56
Allan Reyes <allan.gamer@gmail.com>
2024-07-15 00:17:57 -0600

    feat: Actualizar entrada de kilos en la vista SaleNote

--- src/views/NoteMenu.vue
+++ src/views/NoteMenu.vue
@@ -1,30 +1,32 @@
 <template>
-    <div class="notes-wrapper">
-      <!-- Contenido de tu menú aquí -->
-      <div class="button-container">
-        <div class="button-wrapper">
-          <button @click="goToSaleNote">Nueva nota</button>
-        </div>
-        <div class="button-wrapper">
-          <button @click="goToAddClient">Agregar Cliente</button> <!-- Nuevo botón -->
-        </div>
+  <div class="notes-wrapper">
+    <div class="button-container">
+      <div class="button-wrapper">
+        <button @click="goToSaleNote">Nueva nota</button>
       </div>
-  
-<div class="notes-container">
-  <div class="filter-container">
-  <select v-model="paymentFilter">
-    <option value="all">Todas</option>
-    <option value="paid">Pagadas</option>
-    <option value="unpaid">No Pagadas</option>
-  </select>
-</div>
+      <div class="button-wrapper">
+        <button @click="goToAddClient">Agregar Cliente</button>
+      </div>
+    </div>
+
+    <div class="notes-container">
+      <div class="filter-container">
+        <select v-model="paymentFilter">
+          <option value="all">Todas</option>
+          <option value="paid">Pagadas</option>
+          <option value="unpaid">No Pagadas</option>
+        </select>
+      </div>
       <h2>Notas por Cliente</h2>
       <div v-for="(notes, client) in filteredNotesByClient" :key="client">
         <details>
-          <summary>{{ client }}</summary>
+          <summary>
+            {{ client }} - Deuda Total: {{ totalDebtByClient[client] | currency }} 
+            <span class="note-count">({{ notes.length }})</span>
+          </summary>
           <ul>
             <li v-for="note in notes" :key="note.id">
-              <button @click="goToEditNote(note.id)">
+              <button @click="goToEditNote(note.id)" tabindex="0">
                 Nota {{ note.folio }} - {{ note.currentDate }}
               </button>
             </li>
@@ -34,69 +36,82 @@
     </div>
   </div>
 </template>
-  
-  <script>
-  import { db } from '@/firebase';
-  import { collection, getDocs } from "firebase/firestore";
-  
-  export default {
-    name: 'NoteMenu',
-    data() {
-      return {
-        notesByClient: {},
-        paymentFilter: 'all', // Valor inicial que muestra todas las notas
-       
-      };
+<script>
+import { db } from '@/firebase';
+import { collection, getDocs } from "firebase/firestore";
+
+export default {
+  name: 'NoteMenu',
+  data() {
+    return {
+      notesByClient: {},
+      paymentFilter: 'all', // Valor inicial que muestra todas las notas
+    };
+  },
+  methods: {
+    goToSaleNote() {
+      this.$router.push({ name: 'SaleNote' });
     },
-    methods: {
-      goToSaleNote() {
-        this.$router.push({ name: 'SaleNote' });
-      },
-      goToAddClient() {
-        this.$router.push({ name: 'AddClient' });
-      },
-      async fetchNotes() {
-        try {
-          const querySnapshot = await getDocs(collection(db, 'notes'));
-          const notes = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-  
-          // Organizar las notas por cliente
-          this.notesByClient = notes.reduce((acc, note) => {
-            if (!acc[note.client]) {
-              acc[note.client] = [];
-            }
-            acc[note.client].push(note);
-            return acc;
-          }, {});
-        } catch (error) {
-          console.error('Error fetching notes: ', error);
-        }
-      }, 
-            goToEditNote(noteId) {
-        this.$router.push({ name: 'editar-nota', params: { noteId } });
+    goToAddClient() {
+      this.$router.push({ name: 'AddClient' });
+    },
+    async fetchNotes() {
+      try {
+        const querySnapshot = await getDocs(collection(db, 'notes'));
+        const notes = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+
+        // Organizar las notas por cliente
+        this.notesByClient = notes.reduce((acc, note) => {
+          if (!acc[note.client]) {
+            acc[note.client] = [];
+          }
+          acc[note.client].push(note);
+          return acc;
+        }, {});
+      } catch (error) {
+        console.error('Error fetching notes: ', error);
       }
     },
-    async mounted() {
-      this.fetchNotes();
+    goToEditNote(noteId) {
+      this.$router.push({ name: 'editar-nota', params: { noteId } });
+    }
+  },
+  async mounted() {
+    await this.fetchNotes();
+  },
+  computed: {
+    filteredNotesByClient() {
+      if (this.paymentFilter === 'all') {
+        return this.notesByClient;
+      }
+      const filtered = {};
+      for (const [client, notes] of Object.entries(this.notesByClient)) {
+        filtered[client] = notes.filter(note => 
+          this.paymentFilter === 'paid' ? note.isPaid : !note.isPaid
+        );
+      }
+      return filtered;
     },
-    computed: {
-  filteredNotesByClient() {
-    if (this.paymentFilter === 'all') {
-      return this.notesByClient;
+    totalDebtByClient() {
+      const debtByClient = {};
+      for (const [client, notes] of Object.entries(this.notesByClient)) {
+        debtByClient[client] = notes.reduce((total, note) => {
+          const noteDebt = note.products.reduce((sum, product) => sum + (product.kilos * product.pricePerKilo), 0) - note.abonos.reduce((sum, abono) => sum + abono.monto, 0);
+          return total + (note.isPaid ? 0 : noteDebt);
+        }, 0);
+      }
+      return debtByClient;
     }
-    const filtered = {};
-    for (const [client, notes] of Object.entries(this.notesByClient)) {
-      filtered[client] = notes.filter(note => 
-        this.paymentFilter === 'paid' ? note.isPaid : !note.isPaid
-      );
+  },
+  filters: {
+    currency(value) {
+      if (!value) return '$0.00';
+      return new Intl.NumberFormat('es-MX', { style: 'currency', currency: 'MXN' }).format(value);
     }
-    return filtered;
   }
-},
-  };
-  </script>
-  
-  <style scoped>
+};
+</script>
+<style scoped>
 /* Estilos generales */
 .notes-wrapper {
   padding: 20px;
@@ -114,18 +129,17 @@
 }
 
 button {
-  background-color: #007BFF;
+  background-color: #3760b0;
   color: white;
   border: none;
-  padding: 5px 10px;
-  border-radius: 4px;
+  padding: 10px 20px;
+  border-radius: 20px;
   cursor: pointer;
-  transition: background-color 0.3s; /* Transición suave al cambiar de color */
-  margin: 0 auto; /* Centrado automático en el eje horizontal */
+  transition: background-color 0.3s;
 }
 
 button:hover {
-  background-color: #0056b3; /* Un azul más oscuro para el estado hover */
+  background-color: #2a4a87;
 }
 
 /* Estilos para el contenedor de filtros */
@@ -133,47 +147,103 @@ button:hover {
   margin-bottom: 20px;
 }
 
+.filter-container select {
+  padding: 10px 20px;
+  border: 2px solid #ccc;
+  border-radius: 20px;
+  background-color: white;
+  transition: border-color 0.3s;
+}
+
+.filter-container select:focus {
+  border-color: #3760b0;
+}
+
 /* Estilos para el contenedor de notas */
 .notes-container {
   max-width: 800px;
   margin: 0 auto;
   padding: 20px;
-  background-color: #f9f9f9;
-  border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  background-color: #e8f0fe;
+  border-radius: 20px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 }
 
 .notes-container h2 {
   text-align: center;
-  color: #1552cc;
-  margin-bottom: 20px; /* Espacio después del título */
+  color: #3760b0;
+  margin-bottom: 20px;
 }
 
 /* Estilos para el componente details */
-
 details {
-  margin-bottom: 10px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
+  margin-bottom: 20px;
+  border: 2px solid #ccc;
+  border-radius: 20px;
   padding: 10px;
+  background-color: white;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 }
 
 summary {
   font-weight: bold;
   cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px;
+  border-radius: 20px;
+  background: linear-gradient(135deg, #3760b0, #2a4a87);
+  color: white;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  transition: background 0.3s;
 }
 
 summary:hover {
-  color: #007BFF;
+  background: linear-gradient(135deg, #2a4a87, #3760b0);
+}
+
+/* Estilos para el contador de notas */
+.note-count {
+  background-color: #ffc107;
+  color: #000;
+  border-radius: 50%;
+  padding: 10px 15px;
+  font-size: 14px;
+  font-weight: bold;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  transition: transform 0.3s;
+}
+
+.note-count:hover {
+  transform: scale(1.2);
 }
 
 /* Estilos para la lista de notas */
 ul {
   list-style-type: none;
   padding-left: 20px;
+  margin-top: 10px; /* Agregar margen superior entre el summary y la lista */
 }
 
 li {
-  margin-bottom: 5px;
+  margin-bottom: 10px; /* Aumentar el margen inferior entre los elementos de la lista */
+}
+
+li button {
+  background-color: transparent;
+  border: none;
+  color: #3760b0;
+  text-decoration: underline;
+  cursor: pointer;
+  font-weight: bold;
+  transition: color 0.3s, background-color 0.3s;
+}
+
+li button:hover {
+  color: white;
+  background-color: #2a4a87;
+  border-radius: 5px;
+  padding: 5px 10px;
 }
 </style>
